4de485c1ebc9df536b86fe878d9f99745138a863
Brittney McInnis <brittneymcinnis1@gmail.com>
2023-12-29 12:31:02 -0600

    pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,9 +6,10 @@ import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
-    const { replace } = useRouter();
-    const handleSearch = useDebouncedCallback((term) => {
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
